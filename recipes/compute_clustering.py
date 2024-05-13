9a1a6bc65ba1e83d897f5c62e414b5cd768f5488
cristian.mankoc@symanto.com <cristian.mankoc@symanto.com>
2024-05-13 15:21:14 +0000

    Recipe SAMPLE_APP_SELECT_ANNOTATIONS.compute_clustering saved through notebook notebook editor for compute_clustering

--- recipes/compute_clustering.py
+++ recipes/compute_clustering.py
@@ -48,16 +48,10 @@ for cl in np.unique(clustering):
     # to_review[indices]=Tru
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df_out=pd.DataFrame(var["id_column"])
+df_out=pd.DataFrame({var["id_column"]:df["id"],var["text_column"]:df["text"],"cluster":clustering,"to_annotate":to_review})
 df_out
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-clustering_df = embeddings_df # For this sample code, simply copy input to output
-
 
 # Write recipe outputs
 clustering = dataiku.Dataset("clustering")
