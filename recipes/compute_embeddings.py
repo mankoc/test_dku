2c1728d4780c5ddea1f3f636877508b94c4f54cb
cristian.mankoc@symanto.com <cristian.mankoc@symanto.com>
2024-05-13 13:46:00 +0000

    Recipe SAMPLE_APP_SELECT_ANNOTATIONS.compute_embeddings saved through notebook notebook editor for compute_embeddings

--- recipes/compute_embeddings.py
+++ recipes/compute_embeddings.py
@@ -1,33 +1,38 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-
-# Read recipe inputs
-input_dataset_prepared = dataiku.Dataset("input_dataset_prepared")
-input_dataset_prepared_df = input_dataset_prepared.get_dataframe()
-
-
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-embeddings_df = input_dataset_prepared_df # For this sample code, simply copy input to output
-
-
-# Write recipe outputs
-embeddings = dataiku.Dataset("embeddings")
-embeddings.write_with_schema(embeddings_df)
 import os
 from sentence_transformers import SentenceTransformer
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Read recipe inputs
+input_dataset_prepared = dataiku.Dataset("input_dataset_prepared")
+df = input_dataset_prepared.get_dataframe()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+client=dataiku.api_client()
+project=client.get_project(dataiku.default_project_key())
+variables=project.get_variables()
+vars=variables["standard"]
+vars
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Load pre-trained model
 sentence_transformer_home = os.getenv('SENTENCE_TRANSFORMERS_HOME')
-model_path = os.path.join(sentence_transformer_home, 'DataikuNLP_average_word_embeddings_glove.6B.300d')
+model_path = os.path.join(sentence_transformer_home, vars["embedding_model"])
 model = SentenceTransformer(model_path)
 
-sentences = ["I really like Ice cream", "Brussels sprouts are okay too"]
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+sentences=df["text"].to_list()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # get sentences embeddings
 embeddings = model.encode(sentences)
 embeddings.shape
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Write recipe outputs
+embeddings = dataiku.Dataset("embeddings")
+embeddings.write_with_schema(embeddings_df)
